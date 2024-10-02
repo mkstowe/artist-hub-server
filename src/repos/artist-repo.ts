e3e855dabdf00f8a2d1c4b0402e00f2094f0426b
Michael Stowe <StoweM1@michigan.gov>
2024-10-02 13:30:09 -0400

    More setup

---
+++ src/repos/artist-repo.ts
@@ -0,0 +1,9 @@
+export async function getArtists() {}
+
+export async function getArtist(id: number) {}
+
+export async function createArtist(input: any) {}
+
+export async function updateArtist(id: number, input: any) {}
+
+export async function deleteArtist(id: number) {}
---
+++ src/repos/user-repo.ts
@@ -0,0 +1,9 @@
+export async function getUsers() {}
+
+export async function getUser(id: number) {}
+
+export async function createUser(input: any) {}
+
+export async function updateUser(id: number, input: any) {}
+
+export async function deleteUser(id: number) {}
